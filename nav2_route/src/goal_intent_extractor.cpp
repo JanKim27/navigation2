9ccdbf96b465baa84cd7090fd1bc3b6643644a55
JanKim <sonysame@korea.ac.kr>
2026-09-05 21:22:25 +0900

    Fix concurrent access to Costmap2D::costmap_ (route_server)
    Signed-off-by: JanKim <sonysame@korea.ac.kr>

--- nav2_route/src/goal_intent_extractor.cpp
+++ nav2_route/src/goal_intent_extractor.cpp
@@ -14,6 +14,7 @@
 
 #include <string>
 #include <memory>
+#include <mutex>
 #include <vector>
 
 #include "nav2_route/goal_intent_extractor.hpp"
@@ -156,7 +157,10 @@ GoalIntentExtractor::findStartandGoal(const std::shared_ptr<const GoalT> goal)
   bool enable_search = enable_search_;
   if (enable_search) {
     try {
-      costmap = costmap_subscriber_->getCostmap();
+      auto source_costmap = costmap_subscriber_->getCostmap();
+      std::lock_guard<nav2_costmap_2d::Costmap2D::mutex_t> lock(
+        *source_costmap->getMutex());
+      costmap = std::make_shared<nav2_costmap_2d::Costmap2D>(*source_costmap);
       costmap_frame_id = global_frame_;
     } catch (const std::exception & ex) {
       enable_search = false;
